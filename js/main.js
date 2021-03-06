e17778e8a9ea97af4666b615abf1c42ea89fcda6
Marcos Leonardo Martins <mleonardomartins@gmail.com>
2021-03-06 15:41:22 -0300

    Versão Alpha sem conteúdo ainda

--- js/main.js
+++ js/main.js
@@ -21,3 +21,27 @@ $("#contato").click(function(e) {
     }, 1200);
  
 });
+
+$("#satelites").click(function(e) {
+ 
+    e.preventDefault();
+ 
+    var obj = "#satelites-section";
+ 
+    $('html, body').animate({
+        scrollTop: $(obj).offset().top
+    }, 1200);
+ 
+});
+
+$("#cursos").click(function(e) {
+ 
+    e.preventDefault();
+ 
+    var obj = "#cursos-section";
+ 
+    $('html, body').animate({
+        scrollTop: $(obj).offset().top
+    }, 1200);
+ 
+});
